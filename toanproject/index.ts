5115d0893a45d8ce1e4723f97e05242300547827
hehe <minhthai1403@gmail.com>
2025-03-17 16:53:47 +0700

    moi

--- toanproject/index.ts
+++ toanproject/index.ts
@@ -1,4 +1,13 @@
 console.log("hello ");
 console.log("HELL");
+<<<<<<< HEAD
 console.log("HELL");
 console.log("HELL");
+console.log("HELL");
+console.log("HELL");
+console.log("HELL");
+console.log("HELL");
+console.log("HELL");
+console.log("HELL");
+=======
+>>>>>>> parent of 1eaa110 (update)
